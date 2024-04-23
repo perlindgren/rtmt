d2651dd6d070963393e083671cc063b87df33458
Per Lindgren <per.lindgren@ltu.se>
2024-04-23 17:17:47 +0200

    tests work

--- src/decode.rs
+++ src/decode.rs
@@ -5,7 +5,7 @@ use std::collections::VecDeque;
 #[derive(Debug, Default)]
 pub struct NcDecode {
     in_buf: Vec<i8>,
-    out_buf: VecDeque<i8>,
+    pub out_buf: VecDeque<i8>,
 }
 
 impl NcDecode {
@@ -23,42 +23,44 @@ impl NcDecode {
     }
 
     // scan frame recursively
-    pub fn scan_frame(&mut self, mut p: usize, skip: bool) -> usize {
+    pub fn scan_frame(&mut self, mut p: i32, skip: bool) -> i32 {
         println!("scan_frame p {}, skip {}", p, skip);
-        let offset = self.in_buf[p];
-        let mut offset_end = offset > 0;
-        let mut offset_abs: usize = offset.unsigned_abs() as usize;
-        let mut i: usize = 1; // skip length
-        loop {
-            if offset_end && i == offset_abs {
-                return i;
-            }
-            let data = self.in_buf[p - i];
-            if data == 0 {
-                println!("preemption package at i {}", i);
-                p = self.scan_frame(p - i - 1, true);
-                println!(
-                    "continue at i {}, p {}, offset_abs {}, p - i = {}",
-                    i,
-                    p,
-                    offset_abs,
-                    p - i
-                );
-                continue;
-            }
-            println!("i {}, d {}, offset_abs {}", i, data, offset_abs);
+        let offset = self.in_buf[p as usize];
+        let mut is_end = offset > 0;
+        let mut next: i32 = p - offset.abs() as i32;
+        p -= 1;
 
-            if i == offset_abs && !offset_end {
-                if !skip {
-                    println!("replace sentinel");
-                    self.out_buf.push_front(0);
-                }
-                offset_end = data > 0;
-                offset_abs += data.unsigned_abs() as usize;
-            } else if !skip {
-                self.out_buf.push_front(data);
+        loop {
+            println!("loop: is_end {}, p {}, next {}", is_end, p, next);
+            if is_end && p == next {
+                println!("frame done p {}, frame {:?}", p, self.out_buf);
+                return p;
             }
-            i += 1;
+
+            let data = self.in_buf[p as usize];
+            if data == 0 {
+                println!("preemption package at p {}", p);
+                let new_p = self.scan_frame(p - 1, true);
+                next -= p - new_p;
+                p = new_p;
+                println!("continue");
+                continue;
+            } else {
+                println!("p {}, next {}, data {}", p, next, data);
+
+                if p == next && !is_end {
+                    if !skip {
+                        println!("replace sentinel");
+                        self.out_buf.push_front(0);
+                    }
+                    is_end = data > 0;
+                    next -= data.abs() as i32;
+                } else if !skip {
+                    println!("----------- push data {}", data);
+                    self.out_buf.push_front(data);
+                }
+                p -= 1;
+            }
         }
     }
 
@@ -69,7 +71,8 @@ impl NcDecode {
 
         if data == 0 {
             println!("in_buf {:?}", nc.in_buf);
-            nc.scan_frame(nc.in_buf.len() - 2, false);
+            let r = nc.scan_frame((nc.in_buf.len() - 2) as i32, false);
+            println!("r {}", r);
             true
         } else {
             false
@@ -77,213 +80,3 @@ impl NcDecode {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_ABC() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-        assert!(!nc.decode(66));
-        assert!(!nc.decode(67));
-        assert!(!nc.decode(4)); // length
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [65i8, 66, 67].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_0() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(1));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_00() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(1));
-        assert!(!nc.decode(-1i8));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [0, 0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_A0() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-        assert!(!nc.decode(2));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [65, 0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_A00B0() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-        assert!(!nc.decode(2));
-        assert!(!nc.decode(-1i8));
-        assert!(!nc.decode(66));
-        assert!(!nc.decode(-2i8));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [65, 0, 0, 66, 0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_AaB_preempt() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-        assert!(!nc.decode(97));
-        assert!(!nc.decode(2));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [97].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(66));
-        assert!(!nc.decode(3));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [65, 66].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_AB_preempt() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-
-        assert!(!nc.decode(1));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(66));
-        assert!(!nc.decode(3));
-        assert!(nc.decode(0)); // sentinel
-
-        assert_eq!(nc.out_buf, [65, 66].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_A00_preempt() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-
-        assert!(!nc.decode(1));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(2));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [65, 0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_A00_0_0_preempt() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-
-        assert!(!nc.decode(1));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(2));
-
-        assert!(!nc.decode(1));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(-1i8));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [65, 0, 0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_A0_0_a_preempt() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-
-        assert!(!nc.decode(1));
-
-        assert!(!nc.decode(97));
-        assert!(!nc.decode(2));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [97].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(2));
-        assert!(!nc.decode(-1i8));
-        assert!(nc.decode(0)); // sentinel
-                               // assert_eq!(nc.out_buf, [65, 0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn decode_AD_B_C_preempt() {
-        let mut nc = NcDecode::new();
-        assert!(!nc.decode(65));
-
-        assert!(!nc.decode(66));
-
-        assert!(!nc.decode(67));
-        assert!(!nc.decode(2));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [67].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(2));
-        assert!(nc.decode(0)); // sentinel
-        assert_eq!(nc.out_buf, [66].as_slice());
-        println!("frame {:?}", nc.out_buf);
-        nc.clear();
-
-        assert!(!nc.decode(68));
-        assert!(!nc.decode(3));
-        assert!(nc.decode(0)); // sentinel
-                               // assert_eq!(nc.out_buf, [65, 0].as_slice());
-        println!("frame {:?}", nc.out_buf);
-    }
-}
--- tests/test.rs
+++ tests/test.rs
@@ -1,50 +1,235 @@
 use rtmt::decode::NcDecode;
 use rtmt::encode::NcEncode;
 
+fn valid(de_in: &[i8], de_expected: &[i8]) {
+    println!("de_in {:?}", de_in);
+    let mut de = NcDecode::new();
+    for d in &de_in[..de_in.len() - 1] {
+        de.decode(*d);
+    }
+
+    de.clear();
+    de.decode(de_in[de_in.len() - 1]);
+
+    assert_eq!(de.out_buf, de_expected);
+    println!("frame {:?}", de.out_buf);
+}
+
+fn frame(en: &mut NcEncode, data: &[i8]) -> Vec<i8> {
+    let mut v: Vec<i8> = vec![];
+    en.frame_begin();
+
+    for d in data {
+        v.push(en.encode(*d))
+    }
+
+    v.append(&mut en.frame_end());
+    v
+}
+
 #[test]
 #[allow(non_snake_case)]
 fn encode_ABC() {
     let mut en = NcEncode::new();
-    let mut s = vec![];
-    en.frame_begin();
-
-    s.append(&mut en.encode(65));
-    s.append(&mut en.encode(66));
-    s.append(&mut en.encode(67));
-
-    s.append(&mut en.frame_end());
-    println!("s {:?}", s);
-    assert_eq!(s, [65, 66, 67, 4, 0].as_slice());
-
-    let mut de = NcDecode::new();
-    for d in s {
-        de.decode(d as u8);
-    }
-
-    assert_eq!(de.out_buf, [65u8, 66, 67].as_slice());
-    println!("frame {:?}", de.out_buf);
+    let s = frame(&mut en, &[65, 66, 67]);
+    valid(&s, &[65, 66, 67]);
 }
 
 #[test]
 #[allow(non_snake_case)]
-fn encode_long_preempt() {
+fn encode_0() {
+    let mut en = NcEncode::new();
+    let s = frame(&mut en, &[0]);
+
+    valid(&s, &[0]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A0() {
+    let mut en = NcEncode::new();
+    let s = frame(&mut en, &[65, 0]);
+
+    valid(&s, &[65, 0]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A00B0() {
+    let mut en = NcEncode::new();
+    let s = frame(&mut en, &[65, 0, 0, 66, 0]);
+
+    valid(&s, &[65, 0, 0, 66, 0]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A_a_B_preempt() {
+    let mut en = NcEncode::new();
+    let mut s = vec![];
+
+    en.frame_begin();
+    s.push(en.encode(65));
+
+    en.frame_begin();
+    s.push(en.encode(97));
+    s.append(&mut en.frame_end());
+
+    s.push(en.encode(66));
+    s.append(&mut en.frame_end());
+
+    valid(&s, &[65, 66]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A_B_preempt() {
+    let mut en = NcEncode::new();
+    let mut s = vec![];
+    en.frame_begin();
+    s.push(en.encode(65));
+
+    en.frame_begin();
+    s.append(&mut en.frame_end());
+
+    s.push(en.encode(66));
+    s.append(&mut en.frame_end());
+
+    valid(&s, &[65, 66]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A__B_preempt() {
+    let mut en = NcEncode::new();
+    let mut s = vec![];
+    en.frame_begin();
+    s.push(en.encode(65));
+
+    en.frame_begin();
+
+    en.frame_begin();
+    s.append(&mut en.frame_end());
+
+    s.append(&mut en.frame_end());
+
+    s.push(en.encode(66));
+    s.append(&mut en.frame_end());
+
+    valid(&s, &[65, 66]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A_0_0_preempt() {
+    let mut en = NcEncode::new();
+    let mut s = vec![];
+    en.frame_begin();
+    s.push(en.encode(65));
+
+    en.frame_begin();
+    s.push(en.encode(0));
+    s.append(&mut en.frame_end());
+
+    s.push(en.encode(0));
+    s.append(&mut en.frame_end());
+
+    valid(&s, &[65, 0]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A00_0_0_preempt() {
     let mut nc = NcEncode::new();
     let mut s = vec![];
-    let mut v = vec![];
     nc.frame_begin();
+    s.push(nc.encode(65));
 
-    for i in 0..7 {
-        s.append(&mut nc.encode(65 + i));
-        v.push(65 + i);
-    }
+    nc.frame_begin();
+    s.push(nc.encode(0));
+    s.append(&mut nc.frame_end());
+
+    s.push(nc.encode(0));
+
+    nc.frame_begin();
+    s.push(nc.encode(0));
+    s.append(&mut nc.frame_end());
+
+    s.push(nc.encode(0));
+    s.append(&mut nc.frame_end());
+
+    valid(&s, &[65, 0, 0]);
+}
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_A0_0_a_preempt() {
+    let mut nc = NcEncode::new();
+    let mut s = vec![];
+    nc.frame_begin();
+    s.push(nc.encode(65));
+
+    nc.frame_begin();
+    s.push(nc.encode(0));
+
+    nc.frame_begin();
+    s.push(nc.encode(97));
+    s.append(&mut nc.frame_end());
+
+    s.append(&mut nc.frame_end());
+
     s.append(&mut nc.frame_end());
     println!("s {:?}", s);
 
-    let mut de = NcDecode::new();
-    for d in s {
-        de.decode(d as u8);
-    }
-
-    // assert_eq!(de.out_buf, [65u8, 66, 67].as_slice());
-    println!("frame {:?}", de.out_buf);
+    valid(&s, &[65]);
 }
+
+#[test]
+#[allow(non_snake_case)]
+fn encode_AD_B_C_preempt() {
+    let mut nc = NcEncode::new();
+    let mut s = vec![];
+    nc.frame_begin();
+    s.push(nc.encode(65));
+
+    nc.frame_begin();
+    s.push(nc.encode(66));
+
+    nc.frame_begin();
+    s.push(nc.encode(67));
+    s.append(&mut nc.frame_end());
+
+    s.append(&mut nc.frame_end());
+
+    s.push(nc.encode(68));
+
+    s.append(&mut nc.frame_end());
+
+    valid(&s, &[65, 68])
+}
+
+// assert_eq!(s, [65, 1, 97, 2, 0, -1, 0, 2, -1, 0].as_slice())
+
+// #[test]
+// #[allow(non_snake_case)]
+// fn encode_long_preempt() {
+//     let mut nc = NcEncode::new();
+//     let mut s = vec![];
+//     let mut v = vec![];
+//     nc.frame_begin();
+
+//     for i in 0..7 {
+//         s.append(&mut nc.encode(65 + i));
+//         v.push(65 + i);
+//     }
+//     s.append(&mut nc.frame_end());
+//     println!("s {:?}", s);
+
+//     let mut de = NcDecode::new();
+//     for d in s {
+//         de.decode(d as u8);
+//     }
+
+//     // assert_eq!(de.out_buf, [65u8, 66, 67].as_slice());
+//     println!("frame {:?}", de.out_buf);
+// }
