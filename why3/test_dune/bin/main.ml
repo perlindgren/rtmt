b5da98a6fa307eb8025d99754faddd62a7f92666
Per Lindgren <per.lindgren@ltu.se>
2024-05-04 00:37:52 +0200

    weird

--- why3/test_dune/bin/main.ml
+++ why3/test_dune/bin/main.ml
@@ -1,9 +1,11 @@
 open N_cobs
 open Printf
 
+let rec makeList v i = if i = 0 then [] else i+1000 :: (makeList v (i-1))
+
 let () =
   (*let input = [] in *)
-  let input = [ 0 ] in
+  let input = makeList 41 260 in
   (* let input = [ 41; 0 ] in *)
   (* let input = [ 0; 41 ] in *)
   (* let input = [ 41; 0; 42 ] in *)
@@ -21,4 +23,5 @@ let () =
   let decoded = List.map Z.to_int d in
   List.iter (printf "%d ") decoded;
   print_newline ();
+  printf "Len input %d, len decoded %d \n " (List.length input) (List.length decoded);
   assert (decoded == input)
