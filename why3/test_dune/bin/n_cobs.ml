17570ef17b342b077f46f98a0c205c40e1d571c2
Per Lindgren <per.lindgren@ltu.se>
2024-05-03 15:59:43 +0200

    wip

--- why3/test_dune/bin/n_cobs.ml
+++ why3/test_dune/bin/n_cobs.ml
@@ -1,27 +1,25 @@
-let rec fn_next_0 (l: (Z.t) list) : (Z.t) * ((Z.t) list) =
+let rec rec_encode (l: (Z.t) list) : (Z.t) * ((Z.t) list) =
   match l with
   | [] -> (Z.one, Z.zero :: [] )
   | e :: ll ->
-    (let (n, ln) = fn_next_0 ll in
+    (let (n, ln) = rec_encode ll in
      if Z.equal e Z.zero then (Z.one, n :: ln) else (Z.add Z.one n, e :: ln))
 
-let rec fn_encode (l: (Z.t) list) : (Z.t) list =
-  let (n1, ll) = fn_next_0 l in n1 :: ll
+let encode (l: (Z.t) list) : (Z.t) list =
+  let (n1, ll) = rec_encode l in n1 :: ll
 
-let rec fn_next_decode (l: (Z.t) list) (n2: Z.t) : (Z.t) list =
+let rec rec_decode (n2: Z.t) (l: (Z.t) list) : (Z.t) list =
   match l with
   | [] -> [] 
+  | e :: ([]) -> [] 
   | e :: ll1 ->
-    if Z.equal e Z.zero
-    then [] 
-    else
-      begin
-        if Z.equal n2 Z.one
-        then Z.zero :: fn_next_decode ll1 e
-        else e :: fn_next_decode ll1 (Z.sub n2 Z.one) end
+    begin match Z.equal n2 Z.one with
+    | true -> assert false (* absurd *)
+    | false -> assert false (* absurd *)
+    end
 
-let rec fn_decode (l: (Z.t) list) : (Z.t) list =
+let decode (l: (Z.t) list) : (Z.t) list =
   match l with
-  | [] -> [] 
-  | n2 :: ll1 -> fn_next_decode ll1 n2
+  | [] -> assert false (* absurd *)
+  | cn :: cl -> rec_decode cn cl
 
